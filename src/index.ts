e34ce6d72699c5807f08abacfa4640eaabe02863
yfujisawa <yfujisawa@gladd.jp>
2017-12-25 14:38:42 +0900

    :+1: First Commit.

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { DocBase } from 'node-docbase-sdk/lib/DocBase';
+import { DocBaseReqReadPosts } from 'node-docbase-sdk/lib/DocBaseReqReadPosts';
+import { DocBaseResponse } from 'node-docbase-sdk/lib/DocBaseResponse';
+import { DocBaseReqCreatePost } from 'node-docbase-sdk/lib/DocBaseReqCreatePost';
+
+// Get DocBaseAPI Token from cli.
+// ex.
+//   $ DOC_BASE_API_TOKEN=<DOC_BASE_API_TOKEN> node .
+const DOC_BASE_API_TOKEN = process.env.DOC_BASE_API_TOKEN;
+
+const TEAM_NAME = 'gladd';
+
+const docBase: DocBase = new DocBase(DOC_BASE_API_TOKEN);
+
+// メモの検索API
+// @see https://help.docbase.io/posts/92984
+const reqReadPosts: DocBaseReqReadPosts = <DocBaseReqReadPosts>{};
+reqReadPosts.team = TEAM_NAME;
+reqReadPosts.q = '*';
+reqReadPosts.page = 1;
+reqReadPosts.perPage = 20;
+docBase.readPosts(reqReadPosts).then((reponse: DocBaseResponse) => {
+  console.log(reponse);
+});
+
+// メモの詳細取得API
+// @see https://help.docbase.io/posts/97204
+const reqReadPost: DocBaseReqReadPosts = <DocBaseReqReadPosts>{};
+reqReadPost.team = TEAM_NAME;
+reqReadPost.id = 339417;
+docBase.readPosts(reqReadPost).then((reponse: DocBaseResponse) => {
+  console.log(reponse);
+});
+
+// メモの投稿API
+// @see https://help.docbase.io/posts/92980
+const postReqCreate: DocBaseReqCreatePost = <DocBaseReqCreatePost>{};
+postReqCreate.team = TEAM_NAME;
+postReqCreate.title = 'タイトル';
+postReqCreate.body = 'body';
+postReqCreate.draft = false;
+postReqCreate.notice = false;
+postReqCreate.scope = 'private';
+
+docBase.createPost(postReqCreate).then((reponse: DocBaseResponse) => {
+  console.log(reponse);
+});
+
